e153b777b179a3c8fa6070fb776192c91814b092
Emmanuel COLUSSI <mcolussi@bluewin.ch>
2025-05-08 15:10:37 +0200

    Set security

--- env.sh
+++
@@ -1,5 +0,0 @@
-export HOME_DEMO=$HOME/Documents/App/vault-pod
-export CERT=$HOME_DEMO/ssl
-export VAULT_ADDR=https://vault.demos.oramysql01.com:8201
-export VAULT_TOKEN=AAA5B14A-7B7D-4F0B-9F71-8691A188C4B3
-export VAULT_CACERT=$CERT/ca-cert.pem
